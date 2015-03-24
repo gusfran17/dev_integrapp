c07c229bf2d623b0b7d06172aaae1c2585147459
Syrano <spamero1989@hotmail.com>
2015-03-24 20:13:38 -0300

    Edito Register para que quede comprimido

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -5,16 +5,13 @@ class Register extends CI_Controller {
 
 	
 	function __construct() {
-
 		parent::__construct();
 		$this->load->library('form_validation');
-
    	}
 
 
 
 	public function register(){
-
 		$this->form_validation->set_rules('username', 'Nombre de usuario', 'required|callback_usernamecheck');
 		$this->form_validation->set_rules('name', 'Nombre', 'required|alpha');
 		$this->form_validation->set_rules('lastname', 'Apellido', 'required|alpha');
@@ -22,45 +19,18 @@ class Register extends CI_Controller {
 		$this->form_validation->set_rules('password', 'Contraseña', 'required');
 		$this->form_validation->set_rules('repassword', 'Repetir contraseña', 'required|matches[password]');
 		$this->form_validation->set_rules('terms', 'Términos y condiciones', 'required');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 		if ($this->form_validation->run()){
-
-				$insert = array();
-
+			$insert = array();
 			$insert['email'] = $this->input->post("email");
-
 			$insert['username'] = $this->input->post("username");
-
 			$insert['password'] = $this->encrypt->encode($this->input->post("password"), $this->config->item('encryption_key'));
-
 			$insert['name'] = $this->input->post("name");
-
 			$insert['lastname'] = $this->input->post("lastname");
-
 			//$insert['newsletter'] = "testregistronewsletter";//$this->input->post("newsletter");
-
 			$insert['register_date'] = date("Y-m-d H:i:s");
-
 			$id = $this->register_model->register_user($insert);
-
 			redirect('startApp/routedHome/login');
 		}else{
-
-		
-
 			$this->load->view('templates/template_header');
 			$this->load->view('templates/template_nav');
 			$this->load->view('user/register');
@@ -70,19 +40,14 @@ class Register extends CI_Controller {
 
 	
 	public function usernamecheck(){
-
 		$username = $this->input->post("username");
-
 		return $this->register_model->username_check($username);
-
-			
-
 	}
 
+
+
 	public function emailcheck(){
-
 		$email = $this->input->post("email");
-
 		return 	$this->register_model->email_check($email); 
 	}
 
