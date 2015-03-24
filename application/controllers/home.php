a8c88e12f9431ce01c264122747a06d2c2b9c042
Syrano <spamero1989@hotmail.com>
2015-03-24 20:33:46 -0300

    Agrego Nuevo controlador Home

---
+++ application/controllers/home.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function index(){
+		$this->load->view('templates/template_header');
+		if($this->session->userdata("role")){ 
+			$this->showLoginHome();
+		} else { 
+			$this->showUserHome();
+		}
+		$this->load->view('templates/template_footer');
+	}
+
+
+	private function showUserHome(){
+		$this->load->view('templates/template_nav');
+		$this->load->view('templates/template_home');
+	}
+
+	public function routedHome($data){
+		$this->load->view('templates/template_header');
+		$this->load->view('templates/template_nav');
+		$this->load->view('user/'.$data.'');
+		$this->load->view('templates/template_footer');
+	}
+
+}
+
+
+?>
