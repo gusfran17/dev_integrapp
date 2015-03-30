b9c247feeb77e35d727eef72d92701b27e66c9a6
gusfran17 <gusfran17@gmail.com>
2015-03-30 14:14:57 -0300

    Agrego credito en el perfil de Preoveedor

--- application/views/navs/nav_supplier.php
+++ application/views/navs/nav_supplier.php
@@ -2,7 +2,7 @@
 			        <li><a href="<?php echo base_url(); ?><?php echo $userdata; ?>/routedHome/products">PRODUCTOS</a></li>
 			        <li><a href="<?php echo base_url(); ?><?php echo $userdata; ?>/routedHome/request">SOLICITUDES</a></li>
 			        <li><a href="<?php echo base_url(); ?><?php echo $userdata; ?>/routedHome/auction">SUBASTA</a></li>
-			        <li><a href="<?php echo base_url(); ?><?php echo $userdata; ?>/routedHome/offers">OFERTAS</a></li>
+			        <li><a href="<?php echo base_url(); ?><?php echo $userdata; ?>/routedHome/credit">CREDITO</a></li>
 			        <li><a href="<?php echo base_url(); ?>login/logout">LOGOUT</a></li>			      
 			      </ul>
 			    </div>
