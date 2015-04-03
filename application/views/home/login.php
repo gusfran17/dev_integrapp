5a05602185608c7d2779c4a7e4e0ac29ffd990c4
Syrano <spamero1989@hotmail.com>
2015-04-03 15:30:37 -0300

    Arreglo mergeo de cambios

--- application/views/home/login.php
+++ application/views/home/login.php
@@ -2,7 +2,7 @@
 	<h1>Ingresar a mi cuenta</h1>
 	<article>
 		<?php echo validation_errors(); ?>
-<<<<<<< HEAD
+
 		<div class="container-fluid">
 			<div class="row" id="login-form">
 				<div class="col-md-5 col-sm-7 col-xs-12">
@@ -15,12 +15,6 @@
 							<label for="">Contraseña</label>
 							<input type="password" class="form-control" name="password" placeholder="Ingrese su contraseña">
 						</div>
-					<!-- 	<label for="">Ingresar como:</label>
-						<br>
-						<select name="" id="">
-						<option value="fabricante">Fabricante</option>
-						<option value="ortopedista">Ortopedista</option>
-					</select> -->
 						<div class="form-group">
 							<button type="submit" class="btn btn-default" >Ingresar</button>
 						</div> 
@@ -28,28 +22,11 @@
 				</div> 
 				<div class="col-md-7" >
 					<h3>Registrarse:</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit animi minima ad, saepe possimus numquam. Sunt odio, ratione dolor. Vitae ea omnis modi, fugiat, ducimus a illum minus culpa sed!</p>
-					<a href="<?php echo base_url(); ?>home/routedHome/register">Registrar</a>
+					<p>Si aun no es usuario usted puede <a href="<?php echo base_url(); ?>home/routedHome/register">Registrarse</a></p>
+					<a href="<?php echo base_url(); ?>home/routedHome/register">Olvide mi contraseña</a>
 				</div>
 			</div> 
 		</div> 
 		
-=======
-		<h3>Login:</h3>
-		<br>
-		<form action="<?php echo base_url(); ?>login/authenticate" method="POST">
-			<label for="">Usuario</label>
-			<input type="text" name="username" placeholder="Ingrese usuario o email">
-			<br>
-			<label for="">Contraseña</label>
-			<input type="password" name="password" placeholder="Ingrese su contraseña">
-			<br>
-			<input type="submit">
-		</form>
-		<br>
-		<p>Si aun no es usuario usted puede <a href="<?php echo base_url(); ?>home/routedHome/register">Registrarse</a></p>
-		<a href="<?php echo base_url(); ?>home/routedHome/register">Olvide mi contraseña</a>
->>>>>>> 64847266ed9d26046efcc37fd5c6292963184d62
-
 	</article>
 </section>
