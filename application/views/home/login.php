76d1ca4327c3da6d9aedcf59a198dc777af2d1df
Syrano <spamero1989@hotmail.com>
2015-04-03 10:43:46 -0300

    Arreglo diseño de formularios

--- application/views/home/login.php
+++ application/views/home/login.php
@@ -4,24 +4,24 @@
 	<br>
 	<article>
 		<?php echo validation_errors(); ?>
-
-		<h3>Login:</h3>
-		<br>
-		<br>
 		<form action="<?php echo base_url(); ?>login/authenticate" method="POST">
-			<label for="">Usuario</label>
-			<input type="text" name="username" placeholder="Ingrese usuario o email">
+			<div class="form-group">
+				<label for="">Usuario</label>
+				<input type="text" class="form-control" name="username" placeholder="Ingrese usuario o email">
+			</div>
+			<div class="form-group">
+				<label for="">Contraseña</label>
+				<input type="password" class="form-control" name="password" placeholder="Ingrese su contraseña">
+			</div>
+		<!-- 	<label for="">Ingresar como:</label>
 			<br>
-			<label for="">Contraseña</label>
-			<input type="password" name="password" placeholder="Ingrese su contraseña">
-			<br>
-			<label for="">Ingresar como:</label>
-			<br>
-		<!-- 	<select name="" id="">
+			<select name="" id="">
 			<option value="fabricante">Fabricante</option>
 			<option value="ortopedista">Ortopedista</option>
 		</select> -->
-			<input type="submit">
+			<div class="form-group">
+				<button type="submit" class="btn btn-default" >Ingresar</button>
+			</div> 
 		</form>
 		<h3>Registrarse:</h3>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit animi minima ad, saepe possimus numquam. Sunt odio, ratione dolor. Vitae ea omnis modi, fugiat, ducimus a illum minus culpa sed!</p>
--- application/views/home/register.php
+++ application/views/home/register.php
@@ -6,36 +6,48 @@
 <?php echo validation_errors(); ?>
 
 		<form action="<?php echo base_url(); ?>register/register" method="POST">
-			<label for="">Nombre de Usuario</label>
-			<input type="text" name="username" placeholder="Ingrese su usuario">
-			<br>
-			<label for="">Nombre</label>
-			<input type="text" name="name" placeholder="Ingrese su Nombre">
-			<br>
-			<label for="">Apellido</label>
-			<input type="text" name="lastname" placeholder="Ingrese su Apellido">
-			<br>
-			<label for="">Email</label>
-			<input type="mail" name="email" placeholder="Ingrese su E-Mail">
-			<br>
-			<label for="">Contraseña</label>
-			<input type="password" name="password" placeholder="Ingrese su Contraseña">
-			<br>
-			<label for="">Confirmar Contraseña</label>
-			<input type="password" name="repassword" placeholder="Repita su Contraseña">
-			<br>
-			<label for="">Seleccione Rol</label>
-			<select name="role" id="role">
-				<option value="user">Usuario</option>
-				<option value="distributor">Ortopedista</option>
-				<option value="supplier">Proveedor</option>
-			</select>
-			<br>
-			<input type="checkbox" name="terms">Acepto los terminos y condiciones de integrapp
-			<br>
-			<input type="checkbox" name="newsletter">Envíenme informacion de ofertas y capacitaciones por mail
-			<br>  
-			<input type="submit">
+			<div class="form-group">
+				<label for="">Nombre de Usuario</label>
+				<input type="text" class="form-control" name="username" placeholder="Ingrese su usuario">
+			</div>
+			<div class="form-group">
+				<label for="">Nombre</label>
+				<input type="text" class="form-control" name="name" placeholder="Ingrese su Nombre">
+			</div>
+			<div class="form-group">
+				<label for="">Apellido</label>
+				<input type="text" class="form-control" name="lastname" placeholder="Ingrese su Apellido">
+			</div>
+			<div class="form-group">
+				<label for="">Email</label>
+				<input type="mail" class="form-control" name="email" placeholder="Ingrese su E-Mail">
+			</div>
+			<div class="form-group">
+				<label for="">Contraseña</label>
+				<input type="password" class="form-control" name="password" placeholder="Ingrese su Contraseña">
+			</div>
+			<div class="form-group">
+				<label for="">Confirmar Contraseña</label>
+				<input type="password" class="form-control" name="repassword" placeholder="Repita su Contraseña">
+			</div>
+			<div class="form-group">
+				<label for="">Seleccione Rol</label>
+				<select name="role" class="form-control" id="role">
+					<option value="user">Usuario</option>
+					<option value="distributor">Ortopedista</option>
+					<option value="supplier">Proveedor</option>
+				</select>
+			</div>
+			<div class="form-group">
+				<input type="checkbox" name="terms">Acepto los terminos y condiciones de integrapp
+			</div>
+			<div class="form-group">
+				<input type="checkbox" name="newsletter">Envíenme informacion de ofertas y capacitaciones por mail
+			</div> 
+			<div class="form-group">
+				<!-- <input type="submit" class="btn btn-default"> -->
+				<button type="submit" class="btn btn-default" >Registrarme</button>
+			</div> 
 		</form>
 	</article>
 </section>
