bafa57484e12b0a5347397a7dd5d92b09faa68bc
Syrano <spamero1989@hotmail.com>
2015-04-12 20:15:36 -0300

    Agrego nuevas opciones a barra de menu supplier

--- application/views/navs/nav_supplier.php
+++ application/views/navs/nav_supplier.php
@@ -1,7 +1,5 @@
 <li><a href="<?php echo base_url(); ?>profile/routedHome/profile">MI CUENTA</a></li>
-			        <li><a href="<?php echo base_url(); ?>profile/routedHome/catalog">MI CATALOGO</a></li>
-			        <li><a href="<?php echo base_url(); ?>profile/routedHome/suppliers">PROVEEDORES</a></li>
-			        <li><a href="<?php echo base_url(); ?>profile/routedHome/products">MIS PRODUCTOS</a></li>
+			        <li><a href="<?php echo base_url(); ?>profile/routedHome/products">PRODUCTOS</a></li>
 			        <li><a href="<?php echo base_url(); ?>profile/routedHome/request">SOLICITUDES</a></li>
 			        <li><a href="<?php echo base_url(); ?>profile/routedHome/auction">SUBASTA</a></li>
 			        <li><a href="<?php echo base_url(); ?>profile/routedHome/credit">CREDITO</a></li>
