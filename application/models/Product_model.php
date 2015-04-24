b7a39efbdefc830beb465319957f9e15097d8843
syrano <horacio.estevez@gmail.com>
2015-04-24 09:14:09 -0300

    Vuelvo a agregar primera opcion en el dropdown

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -15,24 +15,25 @@ class Product_model extends CI_Model {
             $query = $this->db->get('categorias');
         }
         $result = $query->result();
+        
+       if($parent != ""){
+            $result_with_home = array();
+            $home_element = new stdClass();
+            $home_element->id = " ";
+            $home_element->name = "Seleccione  sub-categoria...";
+            $home_element->seo_name = "";
+            $home_element->parent_id = null;
+            $result_with_home[] = $home_element;
+            foreach($result as $r){
+                $result_with_home[] = $r;
+            }
+            $result = $result_with_home;
+        }
+
         return $result;
     }
+    
 
-    function get_all_categories($parent=NULL; $tab){
-        $this->db->where("parent_id", $parent);
-        $query = $this->db->get('category');
-        $result = $query->result();
-
-        $tab = $tab."\t";
-
-        foreach($result as $record){
-                $categories = $categories . $tab . $record->name . "\n" . $this->get_categories($record->id,$tab);
-        }
-        return  $categories;
-
-
-    }
-  
 
 
 }
