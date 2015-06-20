247af2a377d5848b9ea629f07270172de03ea9ff
gusfran17 <gusfran17@gmail.com>
2015-06-20 17:54:49 -0300

    Se agrega boton en barra "PEDIDOS"

--- application/views/navs/nav_distributor.php
+++ application/views/navs/nav_distributor.php
@@ -1,7 +1,7 @@
 <li><a href="<?php echo base_url(); ?>profile/account">MI CUENTA</a></li>
 			        <li><a href="<?php echo base_url(); ?>profile/product">PRODUCTOS</a></li>
 			        <li><a href="<?php echo base_url(); ?>profile/suppliers">PROVEEDORES</a></li>
-			        <li><a href="<?php echo base_url(); ?>profile/request">SOLICITUDES</a></li>
+			        <li><a href="<?php echo base_url(); ?>profile/request">PEDIDOS</a></li>
 			        <li><a href="<?php echo base_url(); ?>login/logout">LOGOUT</a></li>			      
 			      </ul>
 			    </div>
