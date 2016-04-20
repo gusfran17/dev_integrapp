f88f30fa9b4550c07324bbb3c9dee6aab7bbc636
Gustavo Franco <tavoArgento@Gustavos-MacBook-Pro.local>
2016-04-20 03:29:57 +0100

    Borro imagenes inecesarias

--- application/logs/log-2016-04-20.php
+++ application/logs/log-2016-04-20.php
@@ -34,3 +34,37 @@ INFO - 2016-04-20 04:27:53 --> Pagination Class Initialized
 INFO - 2016-04-20 04:27:53 --> File loaded: /Applications/XAMPP/xamppfiles/htdocs/dev_integrapp/application/views/home/home.php
 INFO - 2016-04-20 04:27:53 --> Final output sent to browser
 DEBUG - 2016-04-20 04:27:53 --> Total execution time: 0.0287
+INFO - 2016-04-20 04:29:11 --> Config Class Initialized
+INFO - 2016-04-20 04:29:11 --> Hooks Class Initialized
+DEBUG - 2016-04-20 04:29:11 --> UTF-8 Support Enabled
+INFO - 2016-04-20 04:29:11 --> Utf8 Class Initialized
+INFO - 2016-04-20 04:29:11 --> URI Class Initialized
+INFO - 2016-04-20 04:29:11 --> Router Class Initialized
+INFO - 2016-04-20 04:29:11 --> Output Class Initialized
+INFO - 2016-04-20 04:29:11 --> Security Class Initialized
+DEBUG - 2016-04-20 04:29:11 --> Global POST, GET and COOKIE data sanitized
+INFO - 2016-04-20 04:29:11 --> Input Class Initialized
+INFO - 2016-04-20 04:29:11 --> Language Class Initialized
+INFO - 2016-04-20 04:29:11 --> Loader Class Initialized
+INFO - 2016-04-20 04:29:11 --> Helper loaded: url_helper
+INFO - 2016-04-20 04:29:11 --> Database Driver Class Initialized
+INFO - 2016-04-20 04:29:11 --> Session: Class initialized using 'files' driver.
+INFO - 2016-04-20 04:29:11 --> Helper loaded: form_helper
+INFO - 2016-04-20 04:29:11 --> Form Validation Class Initialized
+INFO - 2016-04-20 04:29:11 --> Encrypt Class Initialized
+INFO - 2016-04-20 04:29:11 --> Image Lib Class Initialized
+INFO - 2016-04-20 04:29:11 --> Model Class Initialized
+INFO - 2016-04-20 04:29:11 --> Model Class Initialized
+INFO - 2016-04-20 04:29:11 --> Model Class Initialized
+INFO - 2016-04-20 04:29:11 --> Model Class Initialized
+INFO - 2016-04-20 04:29:11 --> Model Class Initialized
+INFO - 2016-04-20 04:29:11 --> Model Class Initialized
+INFO - 2016-04-20 04:29:11 --> Model Class Initialized
+INFO - 2016-04-20 04:29:11 --> Model Class Initialized
+INFO - 2016-04-20 04:29:11 --> Model Class Initialized
+INFO - 2016-04-20 04:29:11 --> Controller Class Initialized
+INFO - 2016-04-20 04:29:11 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2016-04-20 04:29:11 --> Pagination Class Initialized
+INFO - 2016-04-20 04:29:11 --> File loaded: /Applications/XAMPP/xamppfiles/htdocs/dev_integrapp/application/views/home/home.php
+INFO - 2016-04-20 04:29:11 --> Final output sent to browser
+DEBUG - 2016-04-20 04:29:11 --> Total execution time: 0.0334
